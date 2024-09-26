2d5baeb5172d1febbbca7de999ea48123a295d8c
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-26 15:39:23 -0300

    feat: add BrasilApi service

---
+++ depsmvp.insfrastructure/ExternalServices/BrasilApi.cs
@@ -0,0 +1,37 @@
+using System.Dynamic;
+using System.Text.Json;
+using DepsMvp.Application.DTOs;
+using DepsMvp.Application.Services;
+using depsmvp.domain.Entities.Company;
+
+namespace depsmvp.insfrastructure.ExternalServices;
+
+public class BrasilApi : IBrasilApi
+{
+    public async Task<ResponseGeneric<Company>> GetCompany(string cnpj)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/cnpj/v1/{cnpj}");
+        
+        var response = new ResponseGeneric<Company>();
+        
+        using (var client = new HttpClient())
+        {
+            var responseBrasilApi = await client.SendAsync(request);
+            var contetResponse = await responseBrasilApi.Content.ReadAsStringAsync();
+            var objResponse = JsonSerializer.Deserialize<Company>(contetResponse);
+
+            if (responseBrasilApi.IsSuccessStatusCode)
+            {
+                response.HttpCode = responseBrasilApi.StatusCode;
+                response.ReturnData = objResponse;
+            }
+            else
+            {
+                response.HttpCode = responseBrasilApi.StatusCode;
+                response.ErrorResponse = JsonSerializer.Deserialize<ExpandoObject>(contetResponse);
+            }
+        }
+
+        return response;
+    }
+}
