77c367edf11d515423aeb0762e60880a03885e52
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-26 16:06:50 -0300

    feat: add BrasilApi, CompanyService and CompanyMapping injections

--- depsmvp-web-api/Program.cs
+++ depsmvp-web-api/Program.cs
@@ -1,8 +1,18 @@
+using depsmvp.application.Mappings;
+using DepsMvp.Application.Services;
+using depsmvp.insfrastructure.ExternalServices;
+using depsmvp.insfrastructure.InternalServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ICompanyService, CompanyServices>();
+builder.Services.AddSingleton<IBrasilApi, BrasilApi>();
+
+builder.Services.AddAutoMapper(typeof(CompanyMapping));
 
 var app = builder.Build();
 
