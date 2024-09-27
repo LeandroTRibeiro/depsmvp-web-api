922cf579cae7b5b1f8bf55cab184c454277b13a2
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-27 16:01:34 -0300

    feat: add validator for cnpj

---
+++ depsmvp.application/Validators/CNPJValidator.cs
@@ -0,0 +1,38 @@
+namespace depsmvp.application.Validators;
+
+public class CnpjValidator
+{
+    public static bool IsValidCnpj(string cnpj)
+    {
+        cnpj = cnpj.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+
+        if ((cnpj.Length != 14) || new string(cnpj[0], cnpj.Length) == cnpj)
+        {
+            return false;
+        }
+
+        var cnpjTemp = cnpj.Substring(0, 12);
+        var firstVerifyingDigit = CalculateVerifyingDigit(cnpjTemp);
+        var secondVerifyingDigit = CalculateVerifyingDigit(cnpjTemp + firstVerifyingDigit);
+
+        return cnpj.EndsWith(firstVerifyingDigit.ToString() + secondVerifyingDigit.ToString());
+    }
+
+    private static int CalculateVerifyingDigit(string cnpj)
+    {
+        int[] weight = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+        if (cnpj.Length == 13) 
+        {
+            weight = new int[] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+        }
+
+        int sum = 0;
+        for (int i = 0; i < cnpj.Length; i++)
+        {
+            sum += int.Parse(cnpj[i].ToString()) * weight[i];
+        }
+
+        var remainder = sum % 11;
+        return remainder < 2 ? 0 : 11 - remainder;
+    }
+}
