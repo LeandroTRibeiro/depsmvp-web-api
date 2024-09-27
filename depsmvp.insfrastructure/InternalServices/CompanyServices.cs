5d3a66d0a6bbc4203a3caa5745cb569e3219ae5f
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-27 16:09:24 -0300

    feat: implement CompanyRepository.AddCompanyAsync to GetCompany method, to implement search to db

--- depsmvp.insfrastructure/InternalServices/CompanyServices.cs
+++ depsmvp.insfrastructure/InternalServices/CompanyServices.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using AutoMapper;
 using DepsMvp.Application.DTOs;
 using DepsMvp.Application.Services;
@@ -8,17 +9,24 @@ public class CompanyServices : ICompanyService
 {
     private readonly IMapper _mapper;
     private readonly IBrasilApi _brasilApi;
+    private readonly ICompanyRepository _companyRepository;
 
-    public CompanyServices(IMapper mapper, IBrasilApi brasilApi)
+    public CompanyServices(IMapper mapper, IBrasilApi brasilApi, ICompanyRepository companyRepository)
     {
         _mapper = mapper;
         _brasilApi = brasilApi;
+        _companyRepository = companyRepository;
     }
     
     
     public async Task<ResponseGeneric<CompanyResponse>> GetCompany(string cnpj)
     {
         var company = await _brasilApi.GetCompany(cnpj);
+
+        if (company.HttpCode == HttpStatusCode.OK && company.ReturnData != null)
+        {
+            await _companyRepository.AddCompanyAsync(company.ReturnData);
+        }
         
         return _mapper.Map<ResponseGeneric<CompanyResponse>>(company);
     }
