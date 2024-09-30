bc6dc077767a05fdfaa5669a13ae36af229dcb52
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-30 16:31:21 -0300

    fix: Make OpcaoPeloSimples Nullable

---
+++ depsmvp.insfrastructure/Migrations/20240930175349_MakeOpcaoPeloSimplesNullable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace depsmvp.insfrastructure.Migrations
+{
+    public partial class MakeOpcaoPeloSimplesNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "OpcaoPeloSimples",
+                table: "Companies",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "OpcaoPeloMei",
+                table: "Companies",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "OpcaoPeloSimples",
+                table: "Companies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "OpcaoPeloMei",
+                table: "Companies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+    }
+}
