33fdb9f4d2b9c17a53178868e460ba50fbe74ba4
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-30 16:12:59 -0300

    feat: Add CompanyConsultaRelationship

---
+++ depsmvp.insfrastructure/Migrations/20240927195633_AddCompanyConsultaRelationship.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace depsmvp.insfrastructure.Migrations
+{
+    public partial class AddCompanyConsultaRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Query = table.Column<string>(type: "text", nullable: true),
+                    Document = table.Column<string>(type: "text", nullable: true),
+                    SearchtDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consults_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyConsults",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
+                    ConsultId = table.Column<int>(type: "integer", nullable: false),
+                    AssociatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyConsults", x => new { x.CompanyId, x.ConsultId });
+                    table.ForeignKey(
+                        name: "FK_CompanyConsults_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyConsults_Consults_ConsultId",
+                        column: x => x.ConsultId,
+                        principalTable: "Consults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyConsults_ConsultId",
+                table: "CompanyConsults",
+                column: "ConsultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consults_UserId",
+                table: "Consults",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyConsults");
+
+            migrationBuilder.DropTable(
+                name: "Consults");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
