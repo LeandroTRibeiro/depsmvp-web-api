1559061e6b4a5d4f2b84e228294f3ae9d6a1bf4d
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-30 16:22:36 -0300

    feat: create Consult entity

---
+++ depsmvp.domain/Entities/Consult.cs
@@ -0,0 +1,17 @@
+namespace depsmvp.domain.Entities;
+
+public class Consult
+{
+    public int Id { get; set; }
+    
+    public string? Query { get; set; }
+    
+    public string? Document { get; set; }
+    
+    public DateTime SearchtDate { get; set; }
+    
+    public int UserId { get; set; }
+    
+    public User User { get; set; }
+    
+}
