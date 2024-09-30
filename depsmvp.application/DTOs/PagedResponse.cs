016d8503efb45ad4000eb52f20d0bb910d5e0aa5
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-30 16:32:03 -0300

    feat: create PagedResponse DTO

---
+++ depsmvp.application/DTOs/PagedResponse.cs
@@ -0,0 +1,12 @@
+namespace DepsMvp.Application.DTOs;
+
+public class PagedResponse<T>
+{
+    public T Data { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+}
