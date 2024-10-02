e4ddea3d804817f4057e796485e248f0fefec5f4
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-02 15:46:53 -0300

    feat: add interface to ConsultServices

--- depsmvp.application/Interfaces/Services/IConsultServices.cs
+++ depsmvp.application/Interfaces/Services/IConsultServices.cs
@@ -3,7 +3,7 @@ using depsmvp.domain.Entities;
 
 namespace DepsMvp.Application.Services;
 
-public interface IConsultService
+public interface IConsultServices
 {
     Task<PagedResponse<List<Consult>>> GetAllConsultsAsync(int? limit = null, int pageNumber = 1, int pageSize = 10);
 
