6afd0eed43ada3093f5e6a32315ca183aa492a8a
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-02 15:46:33 -0300

    feat: add interface to CompanyServices

---
+++ depsmvp.application/Interfaces/Services/ICompanyServices.cs
@@ -0,0 +1,8 @@
+using DepsMvp.Application.DTOs;
+
+namespace DepsMvp.Application.Services;
+
+public interface ICompanyServices
+{
+    Task<ResponseGeneric<CompanyResponse>> GetCompany(string cnpj);
+}
