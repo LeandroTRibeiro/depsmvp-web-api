4a534b9c7e599f5d3acc029c8ac5ac9d71cb34ba
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-02 15:51:48 -0300

    feat: add PepsResponse

---
+++ depsmvp.application/DTOs/PepsResponse.cs
@@ -0,0 +1,24 @@
+namespace DepsMvp.Application.DTOs;
+
+public class PepsResponse
+{
+    public string? Cpf { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? SiglaFuncao { get; set; }
+
+    public string? DescricaoFuncao { get; set; }
+
+    public string? NivelFuncao { get; set; }
+
+    public string? CodOrgao { get; set; }
+
+    public string? NomeOrgao { get; set; }
+
+    public string? DtInicioExercicio { get; set; }
+
+    public string? DtFimExercicio { get; set; }
+
+    public string? DtFimCarencia { get; set; }
+}
