475f765b1983382557b53fa9dff5d8120d8ea0da
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-11-20 21:36:40 -0300

    Renamed PostPepsByCpfRequest to GetPepsByCpfRequest

--- depsmvp.application/Requests/GetPepsByCpfRequest.cs
+++ depsmvp.application/Requests/GetPepsByCpfRequest.cs
@@ -4,12 +4,9 @@ namespace depsmvp.application.Requests;
 
 public class GetPepsByCpfRequest
 {
-    [Required]
-    public string Cpf { get; set; }
-    
-    [Required]
-    public string ReferenceDate { get; set; }
-    
-    [Required]
-    public int Interval { get; set; }
+    [Required] public string Cpf { get; set; }
+
+    [Required] public string ReferenceDate { get; set; }
+
+    [Required] public int Interval { get; set; }
 }
